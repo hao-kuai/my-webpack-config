ac62156e84e96e057b1c339128909a6e12db7e3c
张文旗 <1071942338@qq.com>
2020-08-11 15:59:50 +0800

    自动清理 dist 目录

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,9 @@
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   mode: "production",
-  plugins: [new OptimizeCssAssetsPlugin()],
+  plugins: [new CleanWebpackPlugin(), new OptimizeCssAssetsPlugin()],
 });
