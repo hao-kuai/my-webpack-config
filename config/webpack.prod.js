e8e68875f182fbf8d0ca48fc62f72e5d326db85b
张文旗 <1071942338@qq.com>
2020-08-11 17:56:53 +0800

    统计构建耗时

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,18 +3,22 @@ const baseConfig = require("./webpack.base.js");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
 
-module.exports = merge(baseConfig, {
-  mode: "production",
-  devtool: "source-map",
-  plugins: [
-    new CleanWebpackPlugin(),
-    new OptimizeCssAssetsPlugin(),
-    //Make sure that the plugin is after any plugins that add images
-    new ImageminPlugin({
-      pngquant: {
-        quality: "95-100",
-      },
-    }),
-  ],
-});
+module.exports = smp.wrap(
+  merge(baseConfig, {
+    mode: "production",
+    devtool: "source-map",
+    plugins: [
+      new CleanWebpackPlugin(),
+      new OptimizeCssAssetsPlugin(),
+      //Make sure that the plugin is after any plugins that add images
+      new ImageminPlugin({
+        pngquant: {
+          quality: "95-100",
+        },
+      }),
+    ],
+  })
+);
